0706e864183ea0a5784d30687c3546560871f593
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-21 20:18:39 +0530

    Add Count class to count occurrences of a number

---
+++ Count.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+
+ 
+        System.out.println("Enter 10 integers:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        
+        System.out.print("\nEnter number to search: ");
+        int target = sc.nextInt();
+
+        
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+
+       
+        System.out.println("\n" + target + " Occurs" + count + " times");
+
+        sc.close();
+    }
+}
+
